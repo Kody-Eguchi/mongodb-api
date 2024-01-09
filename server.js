6635e5aa403a060931515599ed72044e0190d3fc
Koji Eguchi <koji.eguchi@hotmail.com>
2024-01-08 21:33:11 -0500

    Connect to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 //routes
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.wgpvecg.mongodb.net/?retryWrites=true&w=majority`;
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connect();
 
 app.listen(3000, () => {
   console.log("Server is running on PORT 3000");
